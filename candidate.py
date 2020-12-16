90bd3a587a33c0bfc0ef3ef99407bd5ad5497275
RJ <rvtayal@gatech.edu>
2020-12-16 00:44:11 -0500

    added bounds limits on candidate

--- candidate.py
+++ candidate.py
@@ -2,9 +2,18 @@ import numpy as np
 
 class Candidate:
     def __init__(self, id):
-        self.bounds = np.sort(np.random.uniform(-2, 2, (2,)))
+        self.bounds = [0, 10]
+        while (self.bounds[1] - self.bounds[0]) > 3:
+            self.bounds = np.sort(np.random.uniform(-2, 2, (2,)))
         self.position = np.average(self.bounds)
         self.id = id
 
     def __repr__(self):
         return self.id
+
+    def reset_pos(self):
+        self.position = np.average(self.bounds)
+
+if __name__ == "__main__":
+    c = Candidate("lol")
+    print(c.bounds)
